6543c64dd7f2cb62ef4459570d72a0edf199c294
Manuel Fajardo <m.fajardo.1098@gmail.com>
2022-10-14 20:53:23 -0500

    color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,55 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
+      base: {
+        100: "#F0EBE3",
+        200: "#E1D7C7",
+        300: "#D2C3AB",
+      },
+      "dark-base": {
+        100: "#2C3639",
+        200: "#1F2A2D",
+        300: "#121C1F",
+      },
       primary: {
-        50: "#F4F4F2",
-        100: "#E8E8E8",
-        150: "#FFC7C7",
-        200: "#BBBFCA",
-        300: "#9B9B9B",
+        100: "#E4DCCF",
+        200: "#C9B99E",
+        300: "#AF967D",
+      },
+      "dark-primary": {
+        100: "#3F4E4F",
+        200: "#2E3A3B",
+        300: "#1D2627",
+      },
+      secondary: {
+        100: "#7D9D9C",
+        200: "#5B7B7A",
+        300: "#394B4A",
+      },
+      "dark-secondary": {
+        100: "#A27B5C",
+        200: "#7F5A3C",
+        300: "#5D3A1C",
+      },
+      shadow: {
+        100: "#576F72",
+        200: "#3D4F52",
+        300: "#232F32",
+      },
+      "dark-shadow": {
+        100: "#DCD7C9",
+        200: "#B9B2A3",
+        300: "#96907D",
+      },
+      light: {
+        100: "#F5F5F5",
+        200: "#E8E8E8",
+        300: "#DADADA",
+      },
+      dark: {
+        100: "#1D1D1D",
+        200: "#0F0F0F",
+        300: "#000000",
       },
       blue: {
         50: "#DBDFFD",
